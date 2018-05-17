dd2e26ca81402125acb3fbafa127bb56c72a89f6
kalai850531 <39344405+kalai850531@users.noreply.github.com>
2018-05-17 13:20:33 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,34 +1,3 @@
-<?php
-
-/**
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
-require_once('./LINEBotTiny.php');
-
-
-
-$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
-$channelSecret = getenv('LINE_CHANNEL_SECRET');
-/*$a=array(
-　0=>"母湯",
-　1=>"光頭葛格",
-　2=>"變魔術",
-  3=>"紅心A",
-  4=>"情與義,值千金,刀山去,地獄去,有何憾"
-);*/
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
