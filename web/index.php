25526889ffbbd6aef0ab9ca1fd81745ef7a791d3
kalai850531 <39344405+kalai850531@users.noreply.github.com>
2018-05-17 16:49:55 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -67,10 +67,9 @@ foreach ($client->parseEvents() as $event) {
                         生死怎問
                         笑由人　誰過問誰過問";
                     }
-                    else if (stripos($message['text'], "曲真儀")!=false){   //stripos("Hello world!","WO") ;
+                    else if (stripos("You love php, I love php too!","我")!=false){   //stripos("Hello world!","WO") ;
                      $m_message="是北七";
                     } 
-                     $m_message=strpos("You love php, I love php too!","php");
                 	if($m_message!="")
                 	{
                 		$client->replyMessage(array(
