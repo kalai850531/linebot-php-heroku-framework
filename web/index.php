b327227fca60d909c4b4821a865805862ab5dd26
kalai850531 <39344405+kalai850531@users.noreply.github.com>
2018-05-17 19:08:34 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -33,7 +33,7 @@ foreach ($client->parseEvents() as $event) {
                     if($message['text']=="我"||$message['text']=="主系選修"){
                         $m_message="電子商務概論,行銷管理,服務創新概論.消費者行為,網路行銷,網路廣告";
                     }
-                    else if($message['text']=="我是老二"||$message['text']=="嘎嘎"||$message['text']=="yee"){
+                    else if($message['text']=="我是老二"||$message['text']=="嘎嘎"||$message['text']=="yee"||$message['text']=="甲偉"){
                         if($message['text']=="我是老二"){
                         $m_message="我是低能兒";
                         }
@@ -42,6 +42,9 @@ foreach ($client->parseEvents() as $event) {
                             }
                          else if($message['text']=="yee"){
                             $m_message="你講話最好給我小心一點";
+                            }
+                        else if($message['text']=="yee"){
+                            $m_message="異形,沒有啦,靠北哦";
                             }
                     }
                     else if($message['text']=="屌你老母"||$message['text']=="港仔"){
