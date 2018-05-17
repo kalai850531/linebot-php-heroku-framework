3e9077aee1a1f9e03da00f871d2d6f5573e1756c
kalai850531 <39344405+kalai850531@users.noreply.github.com>
2018-05-17 17:06:47 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -23,7 +23,7 @@ $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 　"1"=>"牛蛙妹妹",
 　"2"=>"紅心A"
 );*/
-$bbbb=rand(0,1);
+$bbbb=rand(0,4);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
