96ab449a2ae136d5d52a5fb021aa9cb32a0463ed
kalai850531 <39344405+kalai850531@users.noreply.github.com>
2018-05-18 01:10:12 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];
-                	if($m_message!="")
+                	if($m_message!="主系選修")
                 	{
                 		$client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
@@ -36,6 +36,19 @@ foreach ($client->parseEvents() as $event) {
                                 'text' => $m_message
                             )
                         )
+                    	));
+                	}
+                    else if($m_message=="主系選修")
+                	{
+                		 $client->replyMessage(array(
+                          'replyToken' => $event['replyToken'],
+                          'messages' => array(
+                            array(
+                           'type' => 'image', // 訊息類型 (圖片)
+                           'originalContentUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 回復圖片
+                           'previewImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg' // 回復的預覽圖片
+                             )
+                             )
                     	));
                 	}
                     break;
