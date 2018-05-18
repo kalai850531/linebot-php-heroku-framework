0244c279006ad35df51d66248fdd280aded49892
kalai850531 <39344405+kalai850531@users.noreply.github.com>
2018-05-18 10:36:47 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -48,14 +48,17 @@ foreach ($client->parseEvents() as $event) {
                     	));
                 	}
               //   else if($message['text']=="畢業門檻"||$message['text']=="主系選修"||$message['text']=="輔系選修"||$message['text']=="畢業門檻"||)
-                	else if (strtolower($message['text']) == "image" || $message['text'] == "圖片"){
+                	else if (strtolower($message['text']) == "image" || $message['text'] == "畢業門檻"){
+                          if($message['text']=="畢業門檻"){
+                              $m_message='https://i.imgur.com/MNdSdjN.png';
+                              }
                           $client->replyMessage(array(
                          'replyToken' => $event['replyToken'],
                          'messages' => array(
                          array(
                          'type' => 'image', // 訊息類型 (圖片)
-                            'originalContentUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 回復圖片
-                          'previewImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg' // 回復的預覽圖片
+                            'originalContentUrl' => $m_message, // 回復圖片
+                          'previewImageUrl' => $m_message' // 回復的預覽圖片
                              )
                               )
                           ));
